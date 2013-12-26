2fa53c5f6a7ee9589d6ae8733d3e4b5d8490a196
Ryan Dervakos <ryandervakos@Ryans-iMac.local>
2013-12-26 15:19:29 +0200

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3700;
+// var port = 3700;
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -16,7 +16,7 @@ app.get("/", function(req, res){
 
 app.use(express.static(__dirname + '/public')); 
 
-var io = require('socket.io').listen(app.listen(port));
+var io = require('socket.io').listen(app.listen(process.env.PORT));
 
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
@@ -24,4 +24,4 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('message', data);
     });
 });
-console.log("Listening on port :" + port);
+console.log("Listening on port :" + process.env.PORT);
