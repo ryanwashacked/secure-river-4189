dc83f4e10aef2aa23180f61833d7d44736a4ad92
Ryan Dervakos <ryandervakos@Ryans-iMac.local>
2013-12-28 18:29:52 +0200

    Added mongo shit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-require('newrelic.js');
 var express = require("express");
 var app = express();
 var port = 3700;
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/chat', function(err){
+	if(err){
+		console.log(err)
+	}
+	else{
+		console.log('Connected to mongodb!');
+	}
+});
+
+var chatSchema = mongoose.Schema({
+	username: String,
+	msg:  String,
+	created_at: {type: Date, default:Date.now}
+});
+
+var Chat = mongoose.model('Message', chatSchema);
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -20,9 +37,29 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(process.env.PORT || port));
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('welcome message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+ 
+    	var newMsg = new Chat({username: data.username, msg: data.message});
+    	newMsg.save(function(err){
+    		if (err) throw err;
+    		 io.sockets.emit('message', data);
+    	})
+       
     });
+
+    var query = Chat.find({});
+
+    query.sort('-created_at').limit(8).exec(function(err,docs){
+    	if(err) throw err;
+
+    	socket.emit('load old msgs', docs);
+    });
+
+    
 });
+
+
+
+
 console.log("Listening on port :" + process.env.PORT || port);
