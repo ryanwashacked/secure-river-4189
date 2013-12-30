deb2b7f00b4bd91acab6dfa1cce8401e56027964
ryanwashacked <ryanwashacked@gmail.com>
2013-12-30 14:10:17 +0200

    Added scroll and notif function

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,7 @@ window.onload = function() {
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
- 
+  
     socket.on('message', function (data) {
         if(data.message) {
 
@@ -16,12 +16,18 @@ window.onload = function() {
         else {
             console.log("There is a problem:", data);
         }
+         $("#content").scrollTop($("#content")[0].scrollHeight);
+        $(function ()
+        {
+             document.title = data.username + " says: " + data.message;
+        });
     });
 
     socket.on('load old msgs', function(docs){
     for (var i = docs.length - 1; i >= 0 ; i--){
         displayMsg(docs[i]);
     }
+       $("#content").scrollTop($("#content")[0].scrollHeight);
 });
 
     function displayMsg(data){
@@ -37,6 +43,9 @@ window.onload = function() {
             socket.emit('send', { message: text, username: name.value });
             field.value = '';
         }
+
+      
+
     }
 
     $(field).bind('keypress', function(e){
